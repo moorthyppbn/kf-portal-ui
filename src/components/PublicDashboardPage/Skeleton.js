b8cb13fe960ef701645c06b795c6cd3209976c28
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2018-10-24 17:33:32 -0400

    Style chart

--- src/components/PublicDashboardPage/Skeleton.js
+++ src/components/PublicDashboardPage/Skeleton.js
@@ -1,104 +1,55 @@
 import React from 'react';
-import { ResponsiveBar } from '@nivo/bar';
 import { Chart1 } from './mockData';
-import * as V from 'victory';
-import { VictoryBar, VictoryStack, VictoryChart, VictoryAxis, VictoryLegend, Bar } from 'victory';
-import { theme, noGrid } from './theme';
+import {
+  VictoryBar,
+  VictoryStack,
+  VictoryChart,
+  VictoryAxis,
+  VictoryLegend,
+  VictoryPortal,
+  LineSegment,
+} from 'victory';
+import { theme } from './theme';
+
+/**
+ * Truncating text and label padding will have to be set for each chart
+ * Text is SVG and therefore can't be aligned using textAlign
+ * We have to use a combination of textAnchor and padding
+ * Little too fuzzy?
+ */
+
+const truncCutoff = 14;
+const trunc = text => (text.length > truncCutoff ? text.substring(0, truncCutoff) + '...' : text);
+
+const data = Chart1.map(d => {
+  return { ...d, id: trunc(d.id) };
+});
 
 const Skeleton = () => {
   return (
-    /*
-  <div style={{ height: '200px' }}>
-    <div style={{ height: '100%', width: '100%' }}>
-      <ResponsiveBar
-        data={Chart1}
-        keys={['probands', 'familyMembers']}
-        margin={{
-          top: 50,
-          right: 130,
-          bottom: 50,
-          left: 60,
-        }}
-        margin={{
-          left: 250,
-        }}
-        maxValue={1200}
-        padding={0.3}
-        layout="horizontal"
-        colorBy={node => (node.id === 'probands' ? '#1f9bb6' : '#e3429b')}
-        borderColor="inherit:darker(1.6)"
-        enableGridX={true}
-        enableGridY={false}
-        enableLabel={false}
-        labelSkipWidth={12}
-        labelSkipHeight={12}
-        labelTextColor="inherit:darker(1.6)"
-        animate={true}
-        motionStiffness={90}
-        motionDamping={15}
-        axisBottom={{
-          orient: 'bottom',
-          tickSize: 0,
-          tickPadding: 5,
-          tickRotation: 0,
-        }}
-        axisLeft={{
-          orient: 'left',
-          tickSize: 0,
-          tickPadding: 50,
-          tickRotation: 0,
-        }}
-        legends={[
-          {
-            dataFrom: 'legendText',
-            anchor: 'bottom',
-            direction: 'row',
-            justify: false,
-            translateX: 0,
-            translateY: 0,
-            itemsSpacing: 40,
-            itemWidth: 100,
-            itemHeight: 20,
-            itemDirection: 'left-to-right',
-            itemOpacity: 0.85,
-            symbolSize: 20,
-            effects: [
-              {
-                on: 'hover',
-                style: {
-                  itemOpacity: 1,
-                },
-              },
-            ],
-          },
-        ]}
-      />{' '}
-    </div>
-  </div>*/
     <div>
-      <VictoryChart
-        barWidth={16}
-        theme={theme}
-        padding={{ left: 220, right: 20 }}
-        domainPadding={{ x: 0, y: 20 }}
-      >
-        <VictoryAxis dependentAxis />
+      <VictoryChart theme={theme} padding={{ left: 220, right: 20 }} domainPadding={20}>
         <VictoryAxis
           crossAxis={false}
           domain={{ x: [0, 1250] }}
           tickValues={[0, 250, 500, 750, 1000, 1250]}
+          padding={50}
+          axisComponent={
+            <VictoryPortal>
+              <LineSegment />
+            </VictoryPortal>
+          }
         />
-        <VictoryStack barWidth={16} colorScale={['#1f9bb6', '#e3429b']} horizontal>
-          <VictoryBar barWidth={16} data={Chart1} x="id" y="probands" />
-          <VictoryBar barWidth={16} data={Chart1} x="id" y="familyMembers" />
+        <VictoryStack colorScale={['#1f9bb6', '#e3429b']} horizontal>
+          <VictoryBar barWidth={16} data={data} x="id" y="probands" />
+          <VictoryBar barWidth={16} data={data} x="id" y="familyMembers" />
         </VictoryStack>
+
+        <VictoryAxis dependentAxis />
         <VictoryLegend
           y={340}
           x={480}
           centerTitle
-          orientation="horizontal"
-          gutter={20}
-          style={{ width: 200, title: { fontSize: 11 } }}
           data={[
             { name: '# Probands', symbol: { fill: '#1f9bb6', type: 'square' } },
             { name: '# Family Members', symbol: { fill: '#e3429b', type: 'square' } },
--- src/components/PublicDashboardPage/theme.js
+++ src/components/PublicDashboardPage/theme.js
@@ -1,28 +1,26 @@
-/*
-  "grayscale" theme (VictoryTheme.grayscale)
-  The grayscale is the default theme.
-  Try changing it. You could start with `colors` or `fontSize`.
-*/
-
 const assign = Object.assign;
 
 // Colors
-const colors = ['#252525', '#525252', '#737373', '#969696', '#bdbdbd', '#d9d9d9', '#f0f0f0'];
-const charcoal = '#252525';
-const grey = '#969696';
-const grid = '#e0e1e6';
+const blue = '#1f9bb6';
+const purple = '#e3429b';
+const textBlack = '#343434';
+const axisGrey = '#a9acbd';
+const gridGrey = '#e0e1e6';
 
 // Typography
 const sansSerif = "'Open Sans', sans-serif";
 const letterSpacing = 'normal';
 const fontSize = 11;
 
+// Different strokes (for different folks)
+const strokeLinecap = 'square';
+const strokeLinejoin = 'square';
+
 // Layout
 const baseProps = {
-  width: 450,
-  height: 300,
-  //  padding: 50,
-  colorScale: colors,
+  width: 500,
+  height: 260,
+  padding: 50,
 };
 
 // Labels
@@ -31,61 +29,59 @@ const baseLabelStyles = {
   fontSize,
   letterSpacing,
   padding: 10,
-  fill: '#343434',
+  fill: textBlack,
   stroke: 'transparent',
 };
-const centeredLabelStyles = assign({ textAnchor: 'middle' }, baseLabelStyles);
 
-// Put it all together...
+//const centeredLabelStyles = { ...{ textAnchor: 'middle' }, ...baseLabelStyles };
+
 export const theme = {
-  area: {
-    style: {
-      data: {
-        fill: charcoal,
-      },
-      labels: centeredLabelStyles,
-    },
-    ...baseProps,
-  },
   independentAxis: {
     style: {
       axis: {},
       ticks: {},
       grid: {
-        stroke: '#a9acbd',
-        strokeWidth: 1,
+        strokeWidth: x => (x === 0 ? 0 : 1),
+        stroke: gridGrey,
+        strokeLinecap,
+        strokeLinejoin,
       },
     },
   },
   dependentAxis: {
     style: {
-      axis: {},
+      axis: {
+        axis: {
+          stroke: axisGrey,
+          strokeWidth: 1.5,
+          strokeLinecap,
+          strokeLinejoin,
+        },
+      },
+      axisLabel: {},
       grid: {
         strokeWidth: 0,
       },
-      tickLabels: {},
+      tickLabels: {
+        textAnchor: 'start',
+        padding: 200,
+      },
     },
   },
   axis: assign(
     {
       style: {
         axis: {
-          fill: 'transparent',
-          strokeWidth: 3,
-          stroke: '#90A4AE',
+          stroke: axisGrey,
+          strokeWidth: 1.5,
+          strokeLinecap,
+          strokeLinejoin,
         },
-        axisLabel: assign({}, centeredLabelStyles, {
-          padding: 25,
-        }),
         grid: {
-          fill: 'transparent',
-          stroke: '#ECEFF1',
-          strokeWidth: 3,
+          strokeWidth: 0,
         },
         ticks: {
-          fill: 'transparent',
-          size: 1,
-          stroke: 'transparent',
+          strokeWidth: 0,
         },
         tickLabels: baseLabelStyles,
       },
@@ -95,158 +91,24 @@ export const theme = {
   bar: assign(
     {
       style: {
-        data: {
-          fill: charcoal,
-          padding: 8,
-          strokeWidth: 0,
-        },
+        data: {},
         labels: baseLabelStyles,
       },
     },
     baseProps,
   ),
-  boxplot: assign(
-    {
-      style: {
-        max: { padding: 8, stroke: charcoal, strokeWidth: 1 },
-        maxLabels: baseLabelStyles,
-        median: { padding: 8, stroke: charcoal, strokeWidth: 1 },
-        medianLabels: baseLabelStyles,
-        min: { padding: 8, stroke: charcoal, strokeWidth: 1 },
-        minLabels: baseLabelStyles,
-        q1: { padding: 8, fill: grey },
-        q1Labels: baseLabelStyles,
-        q3: { padding: 8, fill: grey },
-        q3Labels: baseLabelStyles,
-      },
-      boxWidth: 20,
-    },
-    baseProps,
-  ),
-  candlestick: assign(
-    {
-      style: {
-        data: {
-          stroke: charcoal,
-          strokeWidth: 1,
-        },
-        labels: centeredLabelStyles,
-      },
-      candleColors: {
-        positive: '#ffffff',
-        negative: charcoal,
-      },
-    },
-    baseProps,
-  ),
   chart: baseProps,
-  errorbar: assign(
-    {
-      borderWidth: 8,
-      style: {
-        data: {
-          fill: 'transparent',
-          stroke: charcoal,
-          strokeWidth: 2,
-        },
-        labels: centeredLabelStyles,
-      },
-    },
-    baseProps,
-  ),
-  group: assign(
-    {
-      colorScale: colors,
-    },
-    baseProps,
-  ),
   legend: {
-    colorScale: colors,
-    gutter: 10,
-    orientation: 'vertical',
+    width: 200,
+    gutter: 20,
+    orientation: 'horizontal',
     titleOrientation: 'top',
     style: {
       data: {
-        type: 'circle',
+        type: 'square',
       },
       labels: baseLabelStyles,
-      title: assign({}, baseLabelStyles, { padding: 5 }),
+      title: assign({}, baseLabelStyles, { fontSize: 11, padding: 5 }),
     },
   },
-  line: assign(
-    {
-      style: {
-        data: {
-          fill: 'transparent',
-          stroke: charcoal,
-          strokeWidth: 2,
-        },
-        labels: centeredLabelStyles,
-      },
-    },
-    baseProps,
-  ),
-  pie: {
-    style: {
-      data: {
-        padding: 10,
-        stroke: 'transparent',
-        strokeWidth: 1,
-      },
-      labels: assign({}, baseLabelStyles, { padding: 20 }),
-    },
-    colorScale: colors,
-    width: 400,
-    height: 400,
-    padding: 50,
-  },
-  scatter: assign(
-    {
-      style: {
-        data: {
-          fill: charcoal,
-          stroke: 'transparent',
-          strokeWidth: 0,
-        },
-        labels: centeredLabelStyles,
-      },
-    },
-    baseProps,
-  ),
-  stack: assign(
-    {
-      colorScale: colors,
-    },
-    baseProps,
-  ),
-  tooltip: {
-    style: assign({}, centeredLabelStyles, { padding: 5, pointerEvents: 'none' }),
-    flyoutStyle: {
-      stroke: charcoal,
-      strokeWidth: 1,
-      fill: '#f0f0f0',
-      pointerEvents: 'none',
-    },
-    cornerRadius: 5,
-    pointerLength: 10,
-  },
-  voronoi: assign(
-    {
-      style: {
-        data: {
-          fill: 'transparent',
-          stroke: 'transparent',
-          strokeWidth: 0,
-        },
-        labels: assign({}, centeredLabelStyles, { padding: 5, pointerEvents: 'none' }),
-        flyout: {
-          stroke: charcoal,
-          strokeWidth: 1,
-          fill: '#f0f0f0',
-          pointerEvents: 'none',
-        },
-      },
-    },
-    baseProps,
-  ),
 };
