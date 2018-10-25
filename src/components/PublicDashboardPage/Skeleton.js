a42a12fdc37e8fc6f97ee2877b5568dd17d69a4a
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2018-10-25 17:18:35 -0400

    Add custom renderTick

--- src/components/PublicDashboardPage/Skeleton.js
+++ src/components/PublicDashboardPage/Skeleton.js
@@ -30,144 +30,146 @@ const data = Chart1.map(d => {
 const Skeleton = () => {
   return (
     <div>
-      <div style={{ width: '600px', height: '500px' }}>
-        {<HorizontalBar theme={theme} data={data} />}
+      <div style={{ width: '0', height: '0' }}>
+        {/*<HorizontalBar theme={theme} data={data} />*/}
       </div>
-      <div style={{ width: '600px', height: '500px' }}>{}</div>
-      <div style={{ width: '600px', height: '500px' }}>{}</div>
 
-      {/*
-      <div style={{ width: '800px', height: '700px' }}>
-        <ResponsiveBar
-          data={Chart1}
-          keys={['probands', 'familyMembers']}
-          indexBy="id"
-          margin={{
-            top: 50,
-            right: 130,
-            bottom: 80,
-            left: 260,
-          }}
-          defs={[
-            {
-              id: 'blue',
-              color: 'blue',
-            },
-            {
-              id: 'red',
-              color: 'red',
-            },
-          ]}
-          fill={[
-            {
-              match: {
-                id: 'probands',
+      {
+        <div style={{ width: '800px', height: '700px' }}>
+          <ResponsiveBar
+            data={Chart1}
+            keys={['probands', 'familyMembers']}
+            indexBy="id"
+            margin={{
+              top: 50,
+              right: 10,
+              bottom: 80,
+              left: 260,
+            }}
+            padding={{
+              left: 50,
+            }}
+            /* defs={[
+              //
+              // type is a required attribute
+              //
+              {
+                id: 'blue',
+                color: 'blue',
               },
-              id: 'blue',
-            },
-            {
-              match: {
-                id: 'familyMembers',
+              {
+                id: 'red',
+                color: 'red',
               },
-              id: 'red',
-            },
-          ]}
-          padding={0.3}
-          layout="horizontal"
-          borderColor="inherit:darker(1.6)"
-          axisBottom={{
-            orient: 'bottom',
-            tickSize: 0,
-            tickPadding: 5,
-            tickRotation: 0,
-          }}
-          axisLeft={{
-            orient: 'left',
-            tickSize: 0,
-            tickPadding: 5,
-            tickRotation: 0,
-            renderTick: props => {
-              console.log(props, 'props');
-              const {
-                value: _value,
-                x,
-                y,
-                opacity,
-                rotate,
-                format,
-                lineX,
-                lineY,
-                onClick,
-                textX,
-                textY,
-                textBaseline,
-                textAnchor,
-                theme,
-              } = this.props;
+            ]}
+            fill={[
+              {
+                match: {
+                  id: 'probands',
+                },
+                id: 'blue',
+              },
+              {
+                match: {
+                  id: 'familyMembers',
+                },
+                id: 'red',
+              },
+            ]}*/
+            padding={0.3}
+            layout="horizontal"
+            borderColor="inherit:darker(1.6)"
+            axisBottom={{
+              orient: 'bottom',
+              tickSize: 0,
+              tickPadding: 5,
+              tickRotation: 0,
+              tickValues: [0, 250, 500, 750, 1000, 1250],
+            }}
+            axisLeft={{
+              orient: 'left',
+              tickSize: 0,
+              tickPadding: 5,
+              tickRotation: 0,
+              renderTick: tick => {
+                console.log('tick', tick);
+                const { value, format, key, x, y, theme } = tick;
 
-              let value = _value;
-              if (format !== undefined) {
-                value = format(value);
-              }
+                // Allow custom formatting
+                let renderedValue = value;
+                if (format !== undefined) {
+                  renderedValue = format(value);
+                }
 
-              let gStyle = { opacity };
-              if (onClick) {
-                gStyle['cursor'] = 'pointer';
-              }
+                const xOffset = 160;
 
-              return (
-                <g
-                  transform={`translate(${x},${y})`}
-                  {...(onClick ? { onClick: e => onClick(e, value) } : {})}
-                  style={gStyle}
-                >
-                  <line x1={0} x2={lineX} y1={0} y2={lineY} style={theme.axis.ticks.line} />
-                  <text
-                    alignmentBaseline={textBaseline}
-                    textAnchor={textAnchor}
-                    transform={`translate(${textX},${textY}) rotate(${rotate})`}
-                    style={theme.axis.ticks.text}
-                  >
-                    {value}
-                  </text>
-                </g>
-              );
-            },
-          }}
-          enableGridX={true}
-          enableGridY={true}
-          labelSkipWidth={12}
-          labelSkipHeight={12}
-          labelTextColor="inherit:darker(1.6)"
-          animate={true}
-          motionStiffness={90}
-          motionDamping={15}
-          legends={[
-            {
-              dataFrom: 'keys',
-              anchor: 'bottom',
-              direction: 'row',
-              justify: false,
-              translateX: 0,
-              translateY: 63,
-              itemsSpacing: 2,
-              itemWidth: 100,
-              itemHeight: 20,
-              itemDirection: 'left-to-right',
-              itemOpacity: 0.85,
-              symbolSize: 20,
-              effects: [
-                {
-                  on: 'hover',
-                  style: {
-                    itemOpacity: 1,
+                return (
+                  <g key={key} transform={`translate(${x - xOffset},${y})`}>
+                    <text
+                      textAnchor="start"
+                      alignmentBaseline="middle"
+                      style={theme.axis.ticks.text}
+                    >
+                      {renderedValue}
+                    </text>
+                  </g>
+                );
+              },
+            }}
+            enableGridX={true}
+            gridXValues={[0, 250, 500, 750, 1000, 1250]}
+            maxValue={1350}
+            enableGridY={false}
+            enableLabel={false}
+            labelSkipWidth={12}
+            labelSkipHeight={12}
+            labelTextColor="inherit:darker(1.6)"
+            animate={true}
+            motionStiffness={90}
+            motionDamping={15}
+            theme={{
+              axis: {
+                domain: {
+                  line: {
+                    stroke: '#a9acbe',
+                    strokeWidth: 2,
                   },
                 },
-              ],
-            },
-          ]}
-        />
-        </div>*/}
+              },
+              grid: {
+                line: {
+                  stroke: '#e7e8ec',
+                  strokeWidth: 2,
+                },
+              },
+            }}
+            legends={[
+              {
+                dataFrom: 'keys',
+                anchor: 'bottom',
+                direction: 'row',
+                justify: false,
+                translateX: 0,
+                translateY: 63,
+                itemsSpacing: 2,
+                itemWidth: 100,
+                itemHeight: 20,
+                itemDirection: 'left-to-right',
+                itemOpacity: 0.85,
+                symbolSize: 20,
+                effects: [
+                  {
+                    on: 'hover',
+                    style: {
+                      itemOpacity: 1,
+                    },
+                  },
+                ],
+              },
+            ]}
+          />
+        </div>
+      }
     </div>
   );
 };
