c631e8e33ee1b9f34d75421a54892574f812e572
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2018-11-28 10:12:51 -0500

    basic transition

--- src/uikit/Multicard/index.js
+++ src/uikit/Multicard/index.js
@@ -3,22 +3,24 @@ import React, { Component } from 'react';
 import CardHeader from 'uikit/Card/CardHeader';
 import CardContent from 'uikit/Card/CardContent';
 import { CardWrapper, HeaderWrapper } from 'uikit/Card/styles';
-// import posed from 'react-pose';
+import posed, { PoseGroup } from 'react-pose';
 import LoadingSpinner from 'uikit/LoadingSpinner';
 import TabMenu from './TabMenu';
 import IndexDots from './IndexDots';
 
-// const AnimatedChild = posed.div({
-//   enter: {
-//     y: 0,
-//     opacity: 1,
-//   },
-//   exit: {
-//     y: 50,
-//     opacity: 0,
-//     transition: { duration: 150 },
-//   },
-// });
+//const slideDirection = x => x;
+const offset = 100;
+const duration = 1750 * 4;
+
+const getStartPosition = (selectedTabIndex, tabIndex) => {
+  console.log('selected tab', selectedTabIndex, 'tab', tabIndex);
+  return selectedTabIndex > tabIndex ? '-100%' : '100%';
+};
+
+const AnimatedChild = posed.div({
+  enter: { x: 0, transition: { x: { duration: duration } } },
+  exit: { x: '-100%', transition: { x: { duration: duration } } },
+});
 
 class Multicard extends Component {
   constructor(props) {
@@ -26,58 +28,68 @@ class Multicard extends Component {
 
     this.state = {
       badgeNumber: null,
-      contentIndex: 0,
+      currentTabIndex: 0,
       title: '',
-      loading: false,
+      loading: true,
     };
+
+    this.animatedTabs = [];
 
     this.setBadge = this.setBadge.bind(this);
     this.setTitle = this.setTitle.bind(this);
     this.setIndex = this.setIndex.bind(this);
   }
 
-  /*
   componentDidMount() {
-    // Animation to be added
-    const animatedChildren = React.Children.map(this.props.children, (child, i) => (
-      <AnimatedChild key={i}>{React.cloneElement(child)}</AnimatedChild>
+    this.setTitle();
+
+    console.log('CDM', this.props);
+    const tabs = this.props.tabs;
+    const animatedTabs = tabs.map((tab, i) => (
+      <PoseGroup>
+        <AnimatedChild key={i} tabIndex={i}>
+          {tab.component({
+            setTitle: this.setTitle,
+            setBadge: this.setBadge,
+            setIndex: this.setIndex,
+          })}
+        </AnimatedChild>
+      </PoseGroup>
     ));
-    this.children = animatedChildren;
+
+    this.animatedTabs = animatedTabs;
+    console.log('animatedTabs', animatedTabs);
+    //setInterval(() => this.setState({ currentTabIndex: this.state.currentTabIndex === 0 ? 1 : 0 }), 4000);
     this.setState({ loading: false });
-    console.log('children', animatedChildren);
-    setInterval(() => this.setState({ contentIndex: this.state.contentIndex === 0 ? 1 : 0 }), 1500);
+    console.log('state', this.state);
   }
-  */
 
   setBadge(n) {
     if (n !== this.state.badgeNumber) this.setState({ badgeNumber: n });
   }
 
   setIndex(i) {
-    this.setState({ contentIndex: i });
+    this.setState({ currentTabIndex: i });
   }
 
-  setTitle(title = this.props.tabs[this.state.contentIndex].title) {
+  setTitle(title = this.props.tabs[this.state.currentTabIndex].title) {
     this.setState({ title });
-  }
-
-  componentDidMount() {
-    this.setTitle();
   }
 
   componentDidUpdate(prevProps, prevState) {
     // tab has updated
-    if (prevState.contentIndex !== this.state.contentIndex) {
+    if (prevState.currentTabIndex !== this.state.currentTabIndex) {
       this.setTitle();
     }
   }
 
   render() {
-    const { loading, contentIndex, title, badgeNumber } = this.state;
+    const { loading, currentTabIndex, title, badgeNumber } = this.state;
     const { tabs, inactive, className, scrollable } = this.props;
 
-    const activeTab = tabs[contentIndex];
+    const activeTab = loading ? null : this.animatedTabs[currentTabIndex];
 
+    //console.log('active component', activeTab);
     return (
       <div>
         {loading ? (
@@ -90,21 +102,15 @@ class Multicard extends Component {
                   tabs.map((tab, i) => (
                     <TabMenu
                       key={i}
-                      active={i === contentIndex}
+                      active={i === currentTabIndex}
                       onClick={() => this.setIndex(i)}
                       title={tab.nav}
                     />
                   ))}
               </CardHeader>
             </HeaderWrapper>
-            <CardContent scrollable={scrollable}>
-              {activeTab.component({
-                setTitle: this.setTitle,
-                setBadge: this.setBadge,
-                setIndex: this.setIndex,
-              })}
-            </CardContent>
-            {inactive ? null : <IndexDots index={contentIndex} items={tabs.length} />}
+            <CardContent scrollable={scrollable}>{activeTab}</CardContent>
+            {inactive ? null : <IndexDots index={currentTabIndex} items={tabs.length} />}
           </CardWrapper>
         )}
       </div>
