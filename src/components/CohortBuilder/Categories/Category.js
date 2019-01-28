4e746abfc6a37bf0b1b865d54da8a393ed080731
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-01-28 13:13:10 -0500

    add basic category layout using Dropdown

--- src/components/CohortBuilder/Categories/Category.js
+++ src/components/CohortBuilder/Categories/Category.js
@@ -2,13 +2,45 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'react-emotion';
 import Column from 'uikit/Column';
+import Dropdown from 'uikit/Dropdown';
+import { items } from './mocks';
 
 const Container = styled(Column)`
   flex: 1;
   justify-content: center;
   align-items: center;
   border-right: 1px solid #d4d6dd;
-  border-top: 1px solid ${({ color }) => (color ? color : 'inherit')};
+  border-top: 1px solid ${({ color }) => (color ? color : 'white')};
+  position: relative;
+  white-space: nowrap;
+  z-index: auto;
+`;
+
+const Label = styled('div')``;
+
+const Options = styled('div')`
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  left: 0;
+  top: 100%;
+  cursor: pointer;
+  text-align: left;
+  background-color: white;
+  border-radius: 5px;
+  box-shadow: 0 0 5.9px 0.1px #bbbbbb;
+
+  > div:not(:last-child) {
+    border-bottom: 1px solid #d4d6dd;
+  }
+`;
+
+const ItemWrapper = styled('div')`
+  display: flex;
+  padding: 17px 10px 17px 23px;
+  font-size: 12px;
+  color: #343434;
+  font-weight: 500;
 `;
 
 const Title = styled('h3')`
@@ -20,9 +52,17 @@ const Title = styled('h3')`
 `;
 
 const Category = ({ title, color }) => (
-  <Container>
+  <Dropdown
+    items={items.map((item, i) => (
+      <div>{item.name}</div>
+    ))}
+    ContainerComponent={Container}
+    OptionsContainerComponent={Options}
+    ItemWrapperComponent={ItemWrapper}
+    showArrow={false}
+  >
     <Title> {title}</Title>
-  </Container>
+  </Dropdown>
 );
 
 Category.propTypes = {
