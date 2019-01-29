57766daf651e5db9e9a7ec80e997905be70ae8aa
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-01-29 14:59:29 -0500

    add icons

--- src/components/CohortBuilder/Categories/index.js
+++ src/components/CohortBuilder/Categories/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import styled from 'react-emotion';
-import Column from 'uikit/Column';
 import Search from './Search';
 import Category from './Category';
 import Row from 'uikit/Row';
+import QuickFilterIcon from 'icons/QuickFilterIcon';
+import StudyIcon from 'icons/StudyIcon';
+import BiospecimenIcon from '../../../icons/BiospecimenIcon';
+import ClinicalIcon from '../../../icons/ClinicalIcon';
+import UploadIcon from '../../../icons/UploadIcon';
 
 const Container = styled(Row)`
   height: 72px;
@@ -17,13 +20,23 @@ const Container = styled(Row)`
 const Categories = () => (
   <Container>
     <Search />
-    <Category title="Quick Filters" color={''} />
-    <Category title="Study" color={''} />
+    <Category title="Quick Filters" color="">
+      <QuickFilterIcon fill="#404c9a" />
+    </Category>
+    <Category title="Study" color={''}>
+      <StudyIcon fill="#dd1f2a" />
+    </Category>
     <Category title="Demographic" color={''} />
-    <Category title="Clinical" color={''} />
-    <Category title="Biospecimens" color={''} />
+    <Category title="Clinical" color={''}>
+      <ClinicalIcon fill="#0caceb" />
+    </Category>
+    <Category title="Biospecimens" color={''}>
+      <BiospecimenIcon fill="#f79122" />
+    </Category>
     <Category title="Available Data" color={''} />
-    <Category title="Upload IDs" color={''} />
+    <Category title="Upload IDs" color={''}>
+      <UploadIcon fill="#edb500" />
+    </Category>
   </Container>
 );
 
